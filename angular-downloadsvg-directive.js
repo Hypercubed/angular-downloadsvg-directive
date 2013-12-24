23f99794b8e03004f93edf9f0ab8e52ac4ef2b32
Hypercubed <hypercubed@gmail.com>
2013-12-24 17:09:50 +0900

    Improved api, now just a click directive

--- angular-downloadsvg-directive.js
+++ angular-downloadsvg-directive.js
@@ -1,43 +1,54 @@
 (function () {
 
-  angular.module('hc.downloader', []);
+  var app = angular.module('hc.downloader', []);
 
-  // Original code from https://github.com/densitydesign/raw/blob/master/js/directives.js
-  angular.module('hc.downloader').directive('svgDownloader', function () {
+  // Losely based on code from https://github.com/densitydesign/raw/blob/master/js/directives.js
+  app.factory('svgDownload', function() {
+
+    return function(el) {
+
+      var elm = angular.element(el);
+
+      elm.find('svg')
+        .attr("version", 1.1)
+        .attr("xmlns", "http://www.w3.org/2000/svg");
+
+      var html = elm.html();
+
+      var blob = new Blob([html], { type: "data:image/svg+xml" });
+
+      return {
+        getHtml: function() { return html; },
+        getBlob: function() { return blob; },
+        asSvg: function(filename) { saveAs(blob, filename) }
+      }
+
+    }
+
+  });
+
+  app.directive('svgDownload', ['svgDownload', function (svgDownload) {
     return {
-      restrict: 'AE',
-      replace:true,
+      restrict: 'A',
       scope : {
-        type : '@',
-        source : '@',
-        label : '@',
-        title : '='
+        source : '@svgDownload'
       },
-      template :  '<button class="btn btn-block btn-success push-up" ng-click="download()">{{label}}</button>',
-      link: function postLink(scope, element, attrs) {
+      link: function (scope, element, attrs) {
 
-        scope.download = function(){
-          if(!scope.source || !scope.type) return;
-          if (scope.type == "svg") downloadSVG();
+        element.on('click', function(event) {
+          scope.$apply(function() {
+            download();
+          });
+        });
+
+        function download(){
+          var filename = (encodeURI(attrs.title) || 'untitled') + ".svg";
+          if(!scope.source) return;
+          svgDownload(scope.source).asSvg(filename);
         }
 
-        /* Download SVG */
-        function downloadSVG(){
-
-          var elm = angular.element(scope.source);
-
-          elm.find('svg')
-            .attr("version", 1.1)
-            .attr("xmlns", "http://www.w3.org/2000/svg");
-
-          var html = elm.html();
-
-          var blob = new Blob([html], { type: "data:image/svg+xml" });
-          saveAs(blob, (encodeURI(scope.title) || 'untitled') + ".svg")
-
-        }
       }
     };
-  });
+  }]);
 
 })();
