4952d6e7a1e93d542be475b84a8ebc0bbc3b9a95
J. Harshbarger <hypercubed@gmail.com>
2014-05-12 16:12:47 +0900

    Added example

--- angular-downloadsvg-directive.js
+++ angular-downloadsvg-directive.js
@@ -3,18 +3,26 @@
   var app = angular.module('hc.downloader', []);
 
   // Losely based on code from https://github.com/densitydesign/raw/blob/master/js/directives.js
-  app.factory('svgDownload', function() {
+  app.factory('svgDownload', ['$log', function($log) {
 
     return function(el) {
 
-      var elm = angular.element(el);
+      var svg = angular.element(el);
 
-      elm.find('svg')
+      if (svg.prop('tagName') !== 'svg') {
+        svg = svg.find('svg');
+      }
+
+      if (svg.length < 1) {
+        $log.warn('svgDownload Error: Element ' + el + ' not found');
+        return null;
+      }
+
+      svg
         .attr("version", 1.1)
         .attr("xmlns", "http://www.w3.org/2000/svg");
 
-      var html = elm.html();
-
+      var html = svg[0].outerHTML;
       var blob = new Blob([html], { type: "data:image/svg+xml" });
 
       return {
@@ -25,14 +33,11 @@
 
     };
 
-  });
+  }]);
 
   app.directive('svgDownload', ['svgDownload', function (svgDownload) {
     return {
       restrict: 'A',
-      scope : {
-        source : '@svgDownload'
-      },
       link: function (scope, element, attrs) {
 
         element.on('click', function(event) {
@@ -43,8 +48,8 @@
 
         function download(){
           var filename = encodeURI(attrs.filename || attrs.title || 'untitled') + ".svg";
-          if(!scope.source) return;
-          svgDownload(scope.source).asSvg(filename);
+          var svg = svgDownload(attrs.svgDownload || 'body');
+          if(svg) { svg.asSvg(filename); }
         }
 
       }
--- todo.md
+++ todo.md
@@ -4,3 +4,4 @@ _\( managed using [todo-md](https://github.com/Hypercubed/todo-md) \)_
 
 - [ ] Add tests
 - [ ] Add example
+- [ ] "Looking up elements via selectors is not supported by jqLite!", fix this
